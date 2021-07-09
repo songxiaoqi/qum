0f75fc42469e038e0954a8d0157c3317ad1d4752
songxiaoqi <songxiaoqi@juntu.com>
2021-07-09 14:06:19 +0800

    fix

---
+++ src/main/java/com/quming/config/ThymeleafConfig.java
@@ -0,0 +1,64 @@
+package com.quming.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class ThymeleafConfig implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    /**
+     * 对模板资源进行解析
+     * 模板资源解析器
+     */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver (){
+        //实例化一个
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        //设置spring的上下文
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("");
+        templateResolver.setSuffix("");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(true);
+        return templateResolver;
+    }
+
+    /**
+     * Thymeleaf标准方言解析器
+     */
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        //配置资源解析器的引擎
+        templateEngine.setTemplateResolver(templateResolver());
+        //设置支持EL表达式
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    /**
+     * 视图解析器
+     */
+    @Bean
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        //配置Thymeleaf标准方言解析器
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
+    }
+
+}
